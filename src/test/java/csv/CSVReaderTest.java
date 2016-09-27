eb65461759f2512aa614f3daf0de42d5771e5bc9
r.rastakfard <r.rastakfard@dotin.ir>
2016-09-27 12:46:51 +0330

    Improved pom.xml

--- src/test/java/csv/CSVReaderTest.java
+++ src/test/java/csv/CSVReaderTest.java
@@ -2,6 +2,9 @@ package csv;
 
 import ir.dotin.utils.csv.CSVReader;
 import org.apache.commons.io.IOUtils;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.junit.Assert;
 import org.junit.Test;
 
 import java.io.InputStream;
@@ -12,6 +15,8 @@ import java.util.Map;
  * Created by r.rastakfard on 9/27/2016.
  */
 public class CSVReaderTest {
+
+    private static final Log log = LogFactory.getLog(CSVReaderTest.class);
 
     @Test
     public void testCSVReader() throws Exception {
@@ -24,9 +29,10 @@ public class CSVReaderTest {
         }
         CSVReader obj = new CSVReader(csvText, true);
         List<Map<String, String>> parsedLines = obj.parse();
+        Assert.assertEquals(8,parsedLines.size());
         for (Map<String, String> record : parsedLines) {
             for (String fieldName : record.keySet()) {
-                System.out.print(fieldName + " : " + record.get(fieldName) + " , ");
+               log.info(fieldName + " : " + record.get(fieldName) );
             }
             System.out.println();
         }
